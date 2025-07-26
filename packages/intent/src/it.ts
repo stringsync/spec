56895da4eb1df8b64c7979c3cfa5a72234f299b2
Jared Johnson <jared@jaredjohnson.dev>
2025-07-26 09:25:52 -0400

    scaffold it imperative creator

---
+++ packages/intent/src/it.ts
@@ -0,0 +1,30 @@
+/**
+ * A static class that provides imperatives for expressing intent specifications.
+ *
+ * See https://datatracker.ietf.org/doc/html/rfc2119
+ */
+export class it {
+  private constructor() {
+    throw new Error('it is a static class and cannot be instantiated');
+  }
+
+  static must() {
+    // TODO
+  }
+
+  static mustNot() {
+    // TODO
+  }
+
+  static should() {
+    // TODO
+  }
+
+  static shouldNot() {
+    // TODO
+  }
+
+  static may() {
+    // TODO
+  }
+}
