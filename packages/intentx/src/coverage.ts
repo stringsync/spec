ba8bc90fbfe96a81a94b1bf07d4fe192f8bd2cdc
Jared Johnson <jared@jaredjohnson.dev>
2025-08-14 11:40:17 -0400

    improve coverage function reusability

--- packages/intentx/src/coverage.ts
+++ packages/intentx/src/coverage.ts
@@ -1,31 +1,18 @@
-import { BunIntentServer } from './bun-intent-server';
-import { InMemoryIntentStorage } from './in-memory-intent-storage';
-import { IntentService } from './intent-service';
 import { DEFAULT_INTENT_PORT } from '@stringsync/intent/src/sdk';
-import { BunCommand } from '@stringsync/core/src/command/bun-command';
+import type { IntentServer } from './intent-server';
+import type { Command } from '@stringsync/core/src/command/types';
 
-export async function coverage(args: string[]) {
-  const intentStorage = new InMemoryIntentStorage();
-  const intentService = new IntentService(intentStorage);
-  const intentServer = new BunIntentServer(intentService);
-
-  const command = new BunCommand({
-    cmd: args,
-    env: {
-      ...process.env,
-      INTENT_ROLE: 'coverage',
-    },
-  });
-
+export async function coverage({
+  intentServer,
+  command,
+}: {
+  intentServer: IntentServer;
+  command: Command;
+}) {
   try {
     await intentServer.start(DEFAULT_INTENT_PORT);
     await command.run();
   } finally {
     await intentServer.stop();
   }
-
-  const events = await intentService.getAllIntentEvents();
-  console.log(JSON.stringify(events, null, 2));
-
-  process.exit();
 }
