bbe76ffcbfeb5128469475c1745a75646ff3667b
Jared Johnson <jared@jaredjohnson.dev>
2025-08-23 10:13:51 -0400

    implement markdown command

--- packages/intentx/src/markdown.ts
+++ packages/intentx/src/markdown.ts
@@ -1,3 +1,22 @@
-export async function markdown() {
-  console.warn('[UNIMPLEMENTED] markdown');
+import { Spec } from '@stringsync/intent/src/spec';
+import * as path from 'path';
+
+export async function markdown(input: { path: string; exportedVariableName: string }) {
+  const module = await import(path.resolve(process.cwd(), input.path));
+
+  const value = module[input.exportedVariableName];
+  if (!value) {
+    throw new Error(
+      `variable '${input.exportedVariableName}' not exported in ${input.path}, ` +
+        `found: ${Object.keys(module)
+          .map((key) => `'${key}'`)
+          .join(', ')}`,
+    );
+  }
+
+  if (!(value instanceof Spec)) {
+    throw new Error(`Exported variable '${input.exportedVariableName}' must be a Spec object`);
+  }
+
+  console.log(await value.toMarkdown());
 }
