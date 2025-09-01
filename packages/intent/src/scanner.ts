502b7a846cf602470561b1a0a3dea8fe448732e3
Jared Johnson <jared@jaredjohnson.dev>
2025-09-01 18:36:39 -0400

    move private methods to module scope

--- packages/intent/src/scanner.ts
+++ packages/intent/src/scanner.ts
@@ -58,8 +58,8 @@ export class Scanner {
       const dfs = (node: ts.Node) => {
         if (ts.isCallExpression(node)) {
           for (const target of SCAN_TARGETS) {
-            if (this.isTargetCall(program, node, target)) {
-              const event = this.toIntentEvent(node, sourceFile, target);
+            if (isTargetCall(target, program, node)) {
+              const event = toIntentEvent(target, node, sourceFile);
               events.push(event);
             }
           }
@@ -72,138 +72,138 @@ export class Scanner {
 
     return events;
   }
+}
 
-  private isTargetCall(program: ts.Program, call: ts.CallExpression, target: ScanTarget) {
-    if (ts.isPropertyAccessExpression(call.expression)) {
-      return this.isTargetPropertyAccessExpression(program, call.expression, target);
-    }
+function isTargetCall(target: ScanTarget, program: ts.Program, call: ts.CallExpression) {
+  if (ts.isPropertyAccessExpression(call.expression)) {
+    return isTargetPropertyAccessExpression(target, program, call.expression);
+  }
 
-    if (ts.isElementAccessExpression(call.expression)) {
-      return this.isTargetElementAccessExpression(program, call.expression, target);
-    }
+  if (ts.isElementAccessExpression(call.expression)) {
+    return isTargetElementAccessExpression(target, program, call.expression);
+  }
 
+  return false;
+}
+
+function isTargetPropertyAccessExpression(
+  target: ScanTarget,
+  program: ts.Program,
+  propertyAccess: ts.PropertyAccessExpression,
+): boolean {
+  return (
+    propertyAccess.name.text === target.methodName &&
+    isTargetReceiver(target, program, propertyAccess.expression)
+  );
+}
+
+function isTargetElementAccessExpression(
+  target: ScanTarget,
+  program: ts.Program,
+  elementAccess: ts.ElementAccessExpression,
+): boolean {
+  return (
+    elementAccess.argumentExpression?.getText() === target.methodName &&
+    isTargetReceiver(target, program, elementAccess.expression)
+  );
+}
+
+function isTargetReceiver(target: ScanTarget, program: ts.Program, expression: ts.Expression) {
+  const receiverType = program.getTypeChecker().getTypeAtLocation(expression);
+  if (!receiverType) {
     return false;
   }
 
-  private isTargetPropertyAccessExpression(
-    program: ts.Program,
-    propertyAccess: ts.PropertyAccessExpression,
-    target: ScanTarget,
-  ): boolean {
-    return (
-      propertyAccess.name.text === target.methodName &&
-      this.isTargetReceiver(program, propertyAccess.expression, target)
-    );
+  return isTargetClass(target, program, receiverType);
+}
+
+function isTargetClass(target: ScanTarget, program: ts.Program, type: ts.Type) {
+  const declarations = program
+    .getTypeChecker()
+    .getPropertyOfType(type, target.methodName)
+    ?.getDeclarations();
+  if (!declarations) {
+    return false;
   }
 
-  private isTargetElementAccessExpression(
-    program: ts.Program,
-    elementAccess: ts.ElementAccessExpression,
-    target: ScanTarget,
-  ): boolean {
-    return (
-      elementAccess.argumentExpression?.getText() === target.methodName &&
-      this.isTargetReceiver(program, elementAccess.expression, target)
-    );
+  return declarations
+    .filter((d) => ts.isClassLike(d.parent))
+    .some((d) => {
+      const parent = d.parent as ts.ClassLikeDeclaration;
+      return (
+        parent.name?.getText() === target.className &&
+        // Check class module
+        d.getSourceFile().fileName.includes(target.moduleFileHint)
+      );
+    });
+}
+
+function toIntentEvent(
+  target: ScanTarget,
+  call: ts.CallExpression,
+  sourceFile: ts.SourceFile,
+): IntentEvent {
+  const { line, character } = ts.getLineAndCharacterOfPosition(sourceFile, call.getStart());
+  const callsite = `${sourceFile.fileName}:${line + 1}:${character + 1}`;
+  const firstArg = extractFirstArgText(call.arguments[0], sourceFile);
+
+  if (target.className === 'Spec' && target.methodName === 'impl') {
+    return {
+      type: 'impl',
+      callsite,
+      intentId: firstArg,
+      specId: '<todo>',
+    };
   }
 
-  private isTargetReceiver(program: ts.Program, expression: ts.Expression, target: ScanTarget) {
-    const receiverType = program.getTypeChecker().getTypeAtLocation(expression);
-    if (!receiverType) {
-      return false;
-    }
-
-    return this.isTargetClass(program, receiverType, target);
+  if (target.className === 'Spec' && target.methodName === 'todo') {
+    return {
+      type: 'todo',
+      callsite,
+      intentId: firstArg,
+      specId: '<todo>',
+    };
   }
 
-  private isTargetClass(program: ts.Program, type: ts.Type, target: ScanTarget) {
-    const declarations = program
-      .getTypeChecker()
-      .getPropertyOfType(type, target.methodName)
-      ?.getDeclarations();
-    if (!declarations) {
-      return false;
-    }
-
-    return declarations
-      .filter((d) => ts.isClassLike(d.parent))
-      .some((d) => {
-        const parent = d.parent as ts.ClassLikeDeclaration;
-        return (
-          parent.name?.getText() === target.className &&
-          // Check class module
-          d.getSourceFile().fileName.includes(target.moduleFileHint)
-        );
-      });
+  if (target.className === 'Sdk' && target.methodName === 'spec') {
+    return {
+      type: 'spec',
+      callsite,
+      specId: '<todo>',
+    };
   }
 
-  private toIntentEvent(
-    call: ts.CallExpression,
-    sourceFile: ts.SourceFile,
-    target: ScanTarget,
-  ): IntentEvent {
-    const { line, character } = ts.getLineAndCharacterOfPosition(sourceFile, call.getStart());
-    const callsite = `${sourceFile.fileName}:${line + 1}:${character + 1}`;
-    const firstArg = this.extractFirstArgText(call.arguments[0], sourceFile);
+  throw new Error(`Unhandled target: ${target.className}.${target.methodName}`);
+}
 
-    if (target.className === 'Spec' && target.methodName === 'impl') {
-      return {
-        type: 'impl',
-        callsite,
-        intentId: firstArg,
-        specId: '<todo>',
-      };
-    }
-
-    if (target.className === 'Spec' && target.methodName === 'todo') {
-      return {
-        type: 'todo',
-        callsite,
-        intentId: firstArg,
-        specId: '<todo>',
-      };
-    }
-
-    if (target.className === 'Sdk' && target.methodName === 'spec') {
-      return {
-        type: 'spec',
-        callsite,
-        specId: '<todo>',
-      };
-    }
-
-    throw new Error(`Unhandled target: ${target.className}.${target.methodName}`);
+function extractFirstArgText(arg: ts.Expression | undefined, sourceFile: ts.SourceFile): string {
+  if (!arg) {
+    return '<none>';
   }
 
-  private extractFirstArgText(arg: ts.Expression | undefined, sourceFile: ts.SourceFile): string {
-    if (!arg) {
-      return '<none>';
-    }
-
-    if (ts.isStringLiteral(arg) || ts.isNoSubstitutionTemplateLiteral(arg)) {
-      return arg.text;
-    }
-
-    if (ts.isTemplateExpression(arg) && arg.templateSpans.length === 0) {
-      return arg.head.text;
-    }
-
-    if (ts.isNumericLiteral(arg)) {
-      return arg.text;
-    }
-
-    if (arg.kind === ts.SyntaxKind.TrueKeyword) {
-      return 'true';
-    }
-
-    if (arg.kind === ts.SyntaxKind.FalseKeyword) {
-      return 'false';
-    }
-
-    if (arg.kind === ts.SyntaxKind.NullKeyword) {
-      return 'null';
-    }
-
-    return arg.getText(sourceFile);
+  if (ts.isStringLiteral(arg) || ts.isNoSubstitutionTemplateLiteral(arg)) {
+    return arg.text;
   }
+
+  if (ts.isTemplateExpression(arg) && arg.templateSpans.length === 0) {
+    return arg.head.text;
+  }
+
+  if (ts.isNumericLiteral(arg)) {
+    return arg.text;
+  }
+
+  if (arg.kind === ts.SyntaxKind.TrueKeyword) {
+    return 'true';
+  }
+
+  if (arg.kind === ts.SyntaxKind.FalseKeyword) {
+    return 'false';
+  }
+
+  if (arg.kind === ts.SyntaxKind.NullKeyword) {
+    return 'null';
+  }
+
+  return arg.getText(sourceFile);
 }
