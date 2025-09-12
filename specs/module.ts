d44cb1bba8356a33a63ff9b5dd3dcae06b095619
Jared Johnson <jared@jaredjohnson.dev>
2025-09-12 18:50:39 -0400

    implement spec.module

--- specs/module.ts
+++ specs/module.ts
@@ -1,11 +1,48 @@
-// spec(spec.module): TODO
+import { Markdown } from '../util/markdown';
+import { Scope } from './scope';
+import { Spec } from './spec';
+import { Selector } from './selector';
+
+// spec(spec.module)
 export class Module {
+  private constructor(
+    private path: string,
+    private markdown: Markdown,
+    private scope: Scope,
+    private specs: Spec[] = [],
+  ) {}
+
+  static async load(path: string, scope: Scope): Promise<Module> {
+    const markdown = await Markdown.load(path);
+    return new Module(path, markdown, scope);
+  }
+
   getPath(): string {
-    return '';
+    return this.path;
   }
 
   getName(): string {
-    return '';
+    return this.markdown.getHeader();
+  }
+
+  getContent(): string {
+    return this.markdown.getContent();
+  }
+
+  getSpecs(): Spec[] {
+    return this.specs;
+  }
+
+  withSpecs(specs: Spec[]): Module {
+    return new Module(this.path, this.markdown, this.scope, specs);
+  }
+
+  findSpec(name: string): Spec | null {
+    return this.specs.find((spec) => spec.getName() === name) ?? null;
+  }
+
+  getScope(): Scope {
+    return this.scope;
   }
 
   // spec(spec.validation): TODO
