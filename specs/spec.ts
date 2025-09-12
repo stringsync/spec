e84e2174b97785cc9b38db736b952d609d61d6b1
Jared Johnson <jared@jaredjohnson.dev>
2025-09-12 19:12:43 -0400

    implement spec.spec and spec.tag

--- specs/spec.ts
+++ specs/spec.ts
@@ -1,14 +1,67 @@
-// spec(spec.spec): TODO
+import { Scope } from './scope';
+import { Tag } from './tag';
+
+// spec(spec.spec)
 export class Spec {
-  getModuleName(): string {
-    return '';
+  private scope: Scope;
+  private name: string;
+  private moduleName: string;
+  private path: string;
+  private tags: Tag[];
+  private content: string;
+
+  constructor(init: {
+    scope: Scope;
+    name: string;
+    moduleName: string;
+    path: string;
+    content: string;
+    tags?: Tag[];
+  }) {
+    this.scope = init.scope;
+    this.name = init.name;
+    this.moduleName = init.moduleName;
+    this.path = init.path;
+    this.content = init.content;
+    this.tags = init.tags ?? [];
+  }
+
+  getScope(): Scope {
+    return this.scope;
   }
 
   getName(): string {
-    return '';
+    return this.name;
+  }
+
+  getModuleName(): string {
+    return this.moduleName;
   }
 
   getPath(): string {
-    return '';
+    return this.path;
+  }
+
+  getLocation(): string {
+    return this.path;
+  }
+
+  getTags(): Tag[] {
+    return this.tags;
+  }
+
+  getContent(): string {
+    return this.content;
+  }
+
+  withTags(tags: Tag[]): Spec {
+    return new Spec({
+      scope: this.scope,
+      name: this.name,
+      moduleName: this.moduleName,
+      path: this.path,
+      content: this.content,
+      tags: tags,
+    });
   }
 }
--- specs/tag.ts
+++ specs/tag.ts
@@ -1,14 +1,48 @@
-// spec(spec.tag): TODO
+import { Scope } from './scope';
+
+// spec(spec.tag)
 export class Tag {
-  getModuleName(): string {
-    return '';
+  private name: string;
+  private specName: string;
+  private moduleName: string;
+  private content: string;
+  private path: string;
+
+  constructor(init: {
+    name: string;
+    specName: string;
+    moduleName: string;
+    content: string;
+    path: string;
+  }) {
+    this.name = init.name;
+    this.specName = init.specName;
+    this.moduleName = init.moduleName;
+    this.content = init.content;
+    this.path = init.path;
+  }
+
+  getName(): string {
+    return this.name;
   }
 
   getSpecName(): string {
-    return '';
+    return this.specName;
+  }
+
+  getModuleName(): string {
+    return this.moduleName;
+  }
+
+  getContent(): string {
+    return this.content;
   }
 
   getPath(): string {
-    return '';
+    return this.path;
+  }
+
+  getLocation(): string {
+    return this.path;
   }
 }
