2395ce27b83c225556f3706959073bb2ffd1d8ca
Jared Johnson <jared@jaredjohnson.dev>
2025-09-14 07:57:04 -0400

    implement template

--- templates/template.ts
+++ templates/template.ts
@@ -1,2 +1,23 @@
-// spec(template.core): TODO
-export class Template {}
+import z, { ZodObject, type ZodRawShape } from 'zod';
+
+// spec(template.core)
+export class Template<T extends ZodRawShape> {
+  public readonly schema: ZodObject<T>;
+  private template: string;
+
+  private constructor(template: string, shape: T = {} as T) {
+    this.template = template;
+    this.schema = z.object(shape);
+  }
+
+  render(args: z.infer<typeof this.schema>): string {
+    this.schema.parse(args);
+
+    let prompt = this.template;
+    for (const key of Object.keys(this.schema.shape)) {
+      const value = args[key as keyof typeof args];
+      prompt = prompt.replace(new RegExp(`\{{${key}}\}`, 'g'), String(value));
+    }
+    return prompt;
+  }
+}
