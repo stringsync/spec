716d6d55a7155da410ec3df7d9276b7f7f95090c
Jared Johnson <jared@jaredjohnson.dev>
2025-09-16 20:04:41 -0400

    remove the lint and refine templates

--- constants.ts
+++ constants.ts
@@ -1,19 +1,11 @@
 import { AUDIT_TEMPLATE } from '~/templates/audit-template';
 import { BUILD_TEMPLATE } from '~/templates/build-template';
 import { PLAN_TEMPLATE } from '~/templates/plan-template';
-import { REFINE_TEMPLATE } from '~/templates/refine-template';
-import { LINT_TEMPLATE } from '~/templates/lint-template';
 import type { Template } from '~/templates/template';
 
 const DEFAULT_INCLUDE_PATTERNS = ['**/*'];
 const MUST_EXCLUDE_PATTERNS = ['**/node_modules/**', '**/dist/**', '**/.git/**'];
-const PROMPT_TEMPLATES: Template[] = [
-  AUDIT_TEMPLATE,
-  REFINE_TEMPLATE,
-  LINT_TEMPLATE,
-  PLAN_TEMPLATE,
-  BUILD_TEMPLATE,
-];
+const PROMPT_TEMPLATES: Template[] = [AUDIT_TEMPLATE, PLAN_TEMPLATE, BUILD_TEMPLATE];
 
 export const constants = {
   DEFAULT_INCLUDE_PATTERNS,
