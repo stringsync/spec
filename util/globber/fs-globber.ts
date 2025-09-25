229aff50f972a8e4d165c8b152810ca73e051875
Jared Johnson <jared@jaredjohnson.dev>
2025-09-25 16:19:20 -0400

    add glob to the project

--- util/globber/fs-globber.ts
+++ util/globber/fs-globber.ts
@@ -1,20 +1,31 @@
 import * as fs from 'fs';
+import { glob } from 'glob';
 import type { Scope } from '~/specs/scope';
 import type { Globber } from '~/util/globber/globber';
 
 export class FsGlobber implements Globber {
   async glob(scope: Scope): Promise<string[]> {
-    const paths = new Array<string>();
     const include = scope.getIncludePatterns();
     const exclude = scope.getExcludePatterns();
+    const paths = await glob(include, { ignore: exclude });
 
-    for await (const path of fs.promises.glob(include, { exclude })) {
-      const stat = await fs.promises.stat(path);
-      if (stat.isFile()) {
-        paths.push(path);
+    const isFiles = await Promise.all(paths.map(isFile));
+    const filepaths: string[] = [];
+    for (let i = 0; i < paths.length; i++) {
+      if (isFiles[i]) {
+        filepaths.push(paths[i]);
       }
     }
 
-    return paths;
+    return filepaths;
+  }
+}
+
+async function isFile(path: string): Promise<boolean> {
+  try {
+    const stats = await fs.promises.stat(path);
+    return stats.isFile();
+  } catch {
+    return false;
   }
 }
